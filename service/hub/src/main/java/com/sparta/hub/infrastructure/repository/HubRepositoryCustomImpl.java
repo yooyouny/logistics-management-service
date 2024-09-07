2cab477d73b500898fdd8df64209cd69da6d0c55
jdm2131 <jdm2131@naver.com>
2024-09-07 23:12:01 +0900

    add: Hub 엔티티 #2

---
+++ service/hub/src/main/java/com/sparta/hub/infrastructure/repository/HubRepositoryCustomImpl.java
@@ -0,0 +1,59 @@
+package com.sparta.hub.infrastructure.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.hub.application.dto.HubResponse;
+import com.sparta.hub.application.dto.HubSearchCond;
+import com.sparta.hub.application.mapper.HubMapper;
+import com.sparta.hub.domain.Hub;
+import com.sparta.hub.domain.QHub;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static com.sparta.hub.domain.QHub.*;
+
+public class HubRepositoryCustomImpl implements HubRepositoryCustom {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+    private final HubMapper hubMapper;
+
+    public HubRepositoryCustomImpl(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+        this.hubMapper = new HubMapper();
+    }
+
+    @Override
+    public Page<HubResponse> searchHub(Pageable pageable, HubSearchCond cond) {
+        JPAQuery<Hub> common = queryFactory
+                .selectFrom(hub)
+                .where(
+                        hubNameEq(cond.getName()),
+                        hubAddressEq(cond.getAddress()),
+                        hub.isDelete.eq(false)
+                );
+
+        List<Hub> list = common.offset(pageable.getOffset())
+                .limit(pageable.getPageSize()).fetch();
+
+        List<HubResponse> content = list.stream()
+                .map(hubMapper::toResponse).toList();
+        return PageableExecutionUtils.getPage(content, pageable, common::fetchCount);
+    }
+
+    private BooleanExpression hubNameEq(String name) {
+        return StringUtils.hasText(name) ? hub.hubName.like("%" + name + "%") : null;
+    }
+
+    private BooleanExpression hubAddressEq(String address) {
+        return StringUtils.hasText(address) ? hub.hubAddress.like("%" + address + "%") : null;
+    }
+}
