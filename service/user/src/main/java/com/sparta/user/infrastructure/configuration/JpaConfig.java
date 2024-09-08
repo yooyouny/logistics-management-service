499cfb2b33455db48f02445f393ec1d8099bec8b
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-09 01:54:29 +0900

    add : DDD 기반 구조 세팅

---
+++ service/user/src/main/java/com/sparta/user/infrastructure/configuration/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.user.infrastructure.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
