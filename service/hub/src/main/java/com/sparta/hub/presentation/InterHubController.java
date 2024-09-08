bd256639b2189bd9f9d34cf79f5f3e14f48473b0
jdm2131 <jdm2131@naver.com>
2024-09-08 22:07:57 +0900

    add: Validation 추가 #5

--- service/hub/src/main/java/com/sparta/hub/presentation/InterHubController.java
+++ service/hub/src/main/java/com/sparta/hub/presentation/InterHubController.java
@@ -4,6 +4,7 @@ import com.sparta.hub.application.dto.interhub.InterHubCreateRequest;
 import com.sparta.hub.application.dto.interhub.InterHubResponse;
 import com.sparta.hub.application.service.InterHubService;
 import com.sparta.hub.presentation.dto.ResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class InterHubController {
     private final InterHubService interHubService;
 
     @PostMapping
-    public ResponseEntity<ResponseDto<InterHubResponse>> createInterHubRoute(@RequestBody InterHubCreateRequest requestDto) {
+    public ResponseEntity<ResponseDto<InterHubResponse>> createInterHubRoute(@Valid @RequestBody InterHubCreateRequest requestDto) {
         InterHubResponse route = interHubService.createRoute(requestDto);
         return ResponseEntity.ok(new ResponseDto<>("200", "허브 간 이동정보 생성", route));
     }
