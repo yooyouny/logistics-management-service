81ee7077920b8ddd4741c7bd14275f79c122e8bf
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-10 20:21:10 +0900

    add : Auth 서버에서 jdbc exclude

--- service/auth/src/main/java/com/sparta/auth/AuthApplication.java
+++ service/auth/src/main/java/com/sparta/auth/AuthApplication.java
@@ -2,10 +2,11 @@ package com.sparta.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // jdbc exclude
 public class AuthApplication {
 
   public static void main(String[] args) {
