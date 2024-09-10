9d9e08407291dd4528c27c730ae92e6f2ab7130e
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-10 13:02:01 +0900

    chore : user_server, user_dto 로 모듈화

--- service/user/user_server/src/main/java/com/sparta/user/domain/model/vo/ShippingManagerType.java
+++ service/user/user_server/src/main/java/com/sparta/user/domain/model/vo/ShippingManagerType.java
@@ -1,6 +1,6 @@
 package com.sparta.user.domain.model.vo;
 
 public enum ShippingManagerType {
-    HUB_MOVING_AGENT,
-    COMPANY_DELIVERY_AGENT
+  HUB_MOVING_AGENT,
+  COMPANY_DELIVERY_AGENT
 }
--- service/user/user_server/src/main/java/com/sparta/user/presentation/controller/UserController.java
+++ service/user/user_server/src/main/java/com/sparta/user/presentation/controller/UserController.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserService userService;
+  private final UserService userService;
 }
