16b5a65aa118d4f81cdacda1d4b0217ae50906ed
jdm2131 <jdm2131@naver.com>
2024-09-10 14:37:57 +0900

    add: 허브 간 이동 정보 전체 조회 #13

--- service/hub/src/main/java/com/sparta/hub/infrastructure/repository/interhub/InterHubRepository.java
+++ service/hub/src/main/java/com/sparta/hub/infrastructure/repository/interhub/InterHubRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface InterHubRepository extends JpaRepository<InterHub, UUID> {
+public interface InterHubRepository extends JpaRepository<InterHub, UUID>, InterHubRepositoryCustom {
 }
