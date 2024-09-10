dee6ba6c18cbfd9c7105b95e618e964f34d6aba8
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-11 01:42:04 +0900

    add : user_server 에서 gateway에서 넣은 claim 파싱 후, context로 저장

---
+++ service/user/user_server/src/main/java/com/sparta/user/JwtAuthentication.java
@@ -0,0 +1,54 @@
+package com.sparta.user;
+
+import com.sparta.commons.domain.jwt.JwtClaim;
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public record JwtAuthentication(
+    Long userId,
+    String username,
+    String role
+) implements Authentication {
+
+  public static JwtAuthentication create(JwtClaim claims) {
+    return new JwtAuthentication(claims.getUserId(), claims.getUsername(), claims.getRole());
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singleton(new SimpleGrantedAuthority(this.role()));
+  }
+
+  @Override
+  public Object getCredentials() {
+    return null;
+  }
+
+  @Override
+  public Object getDetails() {
+    return null;
+  }
+
+  @Override
+  public Object getPrincipal() {
+    return JwtClaim.create(userId, username, role);
+  }
+
+  @Override
+  public boolean isAuthenticated() {
+    return true;
+  }
+
+  @Override
+  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+  }
+
+  @Override
+  public String getName() {
+    return username;
+  }
+}
