d3ad97bf1adf84b77f5639c151855df2df92ac75
jdm2131 <jdm2131@naver.com>
2024-09-10 14:38:17 +0900

    fix: 직렬화 #13

--- service/hub/src/main/java/com/sparta/hub/application/dto/interhub/InterHubResponse.java
+++ service/hub/src/main/java/com/sparta/hub/application/dto/interhub/InterHubResponse.java
@@ -1,11 +1,12 @@
 package com.sparta.hub.application.dto.interhub;
 
+import java.io.Serializable;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
-public class InterHubResponse {
+public class InterHubResponse implements Serializable {
 
     private UUID interHubId;
     private UUID departureHubId;
