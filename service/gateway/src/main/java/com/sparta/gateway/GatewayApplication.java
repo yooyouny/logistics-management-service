7f8251946b1ddab4264176ca0d42384b508bdecf
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-11 01:36:06 +0900

    add : gateway filter 추가

--- service/gateway/src/main/java/com/sparta/gateway/GatewayApplication.java
+++ service/gateway/src/main/java/com/sparta/gateway/GatewayApplication.java
@@ -2,8 +2,11 @@ package com.sparta.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
