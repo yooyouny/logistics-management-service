6289fe82f578ee86abfcdced07a71850f7a241dc
jdm2131 <jdm2131@naver.com>
2024-09-11 23:20:38 +0900

    add: interHubStop 데이터 임의 삼입  #13

--- service/hub/hub-service/src/main/java/com/sparta/hub/InterHubStopData.java
+++ service/hub/hub-service/src/main/java/com/sparta/hub/InterHubStopData.java
@@ -2,20 +2,14 @@
 package com.sparta.hub;
 
 import com.sparta.hub.application.dto.interhub.InterHubCreateRequest;
-import com.sparta.hub.application.dto.interhub.InterHubResponse;
-import com.sparta.hub.application.dto.interhub.InterHubStopResponse;
 import com.sparta.hub.application.mapper.HubMapper;
-import com.sparta.hub.application.mapper.InterHubMapper;
 import com.sparta.hub.application.service.InterHubService;
-import com.sparta.hub.domain.Hub;
 import com.sparta.hub.domain.InterHub;
 import com.sparta.hub.domain.InterHubStop;
+import com.sparta.hub.dto.InterHubResponse;
 import com.sparta.hub.infrastructure.repository.InterHubStopRepository;
 import com.sparta.hub.infrastructure.repository.interhub.InterHubRepository;
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import java.beans.PropertyVetoException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -79,7 +73,7 @@ public class InterHubStopData {
           ,UUID.fromString("31092785-cc0f-45c1-9952-10e98e0abb0b"),236L, 276.2);
       InterHubStop interHubStop5 = new InterHubStop(interHub1, 2, UUID.fromString("31092785-cc0f-45c1-9952-10e98e0abb0b")
           ,UUID.fromString("9e602412-83ea-4b00-b779-09f85b7c0143"),57L, 67.2);
-      InterHubStop interHubStop6 = new InterHubStop(interHub, 3, UUID.fromString("9e602412-83ea-4b00-b779-09f85b7c0143")
+      InterHubStop interHubStop6 = new InterHubStop(interHub1, 3, UUID.fromString("9e602412-83ea-4b00-b779-09f85b7c0143")
           ,UUID.fromString("2107780c-f197-4a45-aefe-801937b6e3b9"),21L, 24.5);
 
       interHubStop4.addStop(interHub1);
@@ -89,6 +83,47 @@ public class InterHubStopData {
       interHubStopRepository.save(interHubStop4);
       interHubStopRepository.save(interHubStop5);
       interHubStopRepository.save(interHubStop6);
+
+      List<InterHubResponse> interHubList1 = interHubService.createRoute(
+          new InterHubCreateRequest(UUID.fromString("168b0282-5c36-461a-ab21-851341bc7ee7"),
+              UUID.fromString("b45e5bca-289f-4c7c-904a-ad397295534e")));
+      List<UUID> idList1 = new ArrayList<>();
+      for (InterHubResponse interHubResponse : interHubList1) {
+        UUID interHubId = interHubResponse.getInterHubId();
+        idList1.add(interHubId);
+      }
+      UUID uuid2 = idList1.get(0);
+      InterHub interHub2 = interHubRepository.findById(uuid2).orElse(null);
+
+      InterHubStop interHubStopA = new InterHubStop(interHub2, 1, UUID.fromString("39bddc22-3e38-4bbd-9ac9-6c22c746f125")
+          ,UUID.fromString("168b0282-5c36-461a-ab21-851341bc7ee7"),108L, 127.0);
+      InterHubStop interHubStopB = new InterHubStop(interHub2, 2, UUID.fromString("779d0d88-68f6-45e8-8fff-5cf51c70f72c")
+          ,UUID.fromString("39bddc22-3e38-4bbd-9ac9-6c22c746f125"),87L, 101.6);
+      InterHubStop interHubStopC = new InterHubStop(interHub2, 3, UUID.fromString("a0159d31-fb3c-49f9-bd2f-7f1b4488b4b8")
+          ,UUID.fromString("779d0d88-68f6-45e8-8fff-5cf51c70f72c"),83L, 97.4);
+      InterHubStop interHubStopD = new InterHubStop(interHub2, 4, UUID.fromString("b45e5bca-289f-4c7c-904a-ad397295534e")
+          ,UUID.fromString("a0159d31-fb3c-49f9-bd2f-7f1b4488b4b8"), 120L,  140.9);
+
+      UUID uuid3 = idList1.get(1);
+      InterHub interHub3 = interHubRepository.findById(uuid3).orElse(null);
+
+      InterHubStop interHubStopH = new InterHubStop(interHub3, 1, UUID.fromString("a0159d31-fb3c-49f9-bd2f-7f1b4488b4b8")
+          ,UUID.fromString("b45e5bca-289f-4c7c-904a-ad397295534e"), 120L,  140.9);
+      InterHubStop interHubStopG = new InterHubStop(interHub3, 2, UUID.fromString("779d0d88-68f6-45e8-8fff-5cf51c70f72c")
+          ,UUID.fromString("a0159d31-fb3c-49f9-bd2f-7f1b4488b4b8"),83L, 97.4);
+      InterHubStop interHubStopF = new InterHubStop(interHub3, 3, UUID.fromString("39bddc22-3e38-4bbd-9ac9-6c22c746f125")
+          ,UUID.fromString("779d0d88-68f6-45e8-8fff-5cf51c70f72c"),87L, 101.6);
+      InterHubStop interHubStopE = new InterHubStop(interHub3, 4, UUID.fromString("168b0282-5c36-461a-ab21-851341bc7ee7")
+          ,UUID.fromString("39bddc22-3e38-4bbd-9ac9-6c22c746f125"),108L, 127.0);
+
+      interHubStopRepository.save(interHubStopA);
+      interHubStopRepository.save(interHubStopB);
+      interHubStopRepository.save(interHubStopC);
+      interHubStopRepository.save(interHubStopD);
+      interHubStopRepository.save(interHubStopE);
+      interHubStopRepository.save(interHubStopF);
+      interHubStopRepository.save(interHubStopG);
+      interHubStopRepository.save(interHubStopH);
 
     }
   }
