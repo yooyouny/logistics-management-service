322b9853553e348bb844f1af14b417ac7284d702
jdm2131 <jdm2131@naver.com>
2024-09-11 22:05:38 +0900

    add: interHubStop #13

---
+++ service/hub/hub-service/src/main/java/com/sparta/hub/infrastructure/repository/InterHubStopRepository.java
@@ -0,0 +1,7 @@
+package com.sparta.hub.infrastructure.repository;
+
+import com.sparta.hub.domain.InterHubStop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterHubStopRepository extends JpaRepository<InterHubStop, Long> {
+}
