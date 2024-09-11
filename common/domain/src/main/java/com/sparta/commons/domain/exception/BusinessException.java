cf7cfa237844b5c924f3c47441a9512c36b725db
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-11 18:15:01 +0900

    refactor: BusinessException, ErrorCode 공통모듈로 추가 - 오더서버의 에러코드 구현체인 OrderErrorCode 정의

---
+++ common/domain/src/main/java/com/sparta/commons/domain/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.sparta.commons.domain.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}
