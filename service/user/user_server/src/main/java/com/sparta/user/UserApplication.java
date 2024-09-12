70b566b8eb07179df63275b8318d022b729984ca
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-12 18:06:59 +0900

    fix : 페이징 관련 경고 제거

--- service/user/user_server/src/main/java/com/sparta/user/UserApplication.java
+++ service/user/user_server/src/main/java/com/sparta/user/UserApplication.java
@@ -1,8 +1,12 @@
 package com.sparta.user;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class UserApplication {
 
