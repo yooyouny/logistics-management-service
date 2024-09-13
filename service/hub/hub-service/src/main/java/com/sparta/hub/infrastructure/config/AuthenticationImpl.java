f5921ff87dac13c12b16acd61f909ea907d219fe
jdm2131 <jdm2131@naver.com>
2024-09-14 01:20:35 +0900

    add: security / auditing 설정 #34

---
+++ service/hub/hub-service/src/main/java/com/sparta/hub/infrastructure/config/AuthenticationImpl.java
@@ -0,0 +1,49 @@
+package com.sparta.hub.infrastructure.config;
+
+import com.sparta.commons.domain.jwt.JwtClaim;
+import java.util.Collection;
+import java.util.Collections;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public record AuthenticationImpl(Long userId, String username, String role)
+    implements Authentication {
+
+  public static AuthenticationImpl create(JwtClaim claims) {
+    return new AuthenticationImpl(claims.getUserId(), claims.getUsername(), claims.getRole());
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.singleton(new SimpleGrantedAuthority(this.role()));
+  }
+
+  @Override
+  public Object getCredentials() {
+    return null;
+  }
+
+  @Override
+  public Object getDetails() {
+    return null;
+  }
+
+  @Override
+  public Object getPrincipal() {
+    return JwtClaim.create(userId, username, role);
+  }
+
+  @Override
+  public boolean isAuthenticated() {
+    return true;
+  }
+
+  @Override
+  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {}
+
+  @Override
+  public String getName() {
+    return username;
+  }
+}
---
+++ service/hub/hub-service/src/main/java/com/sparta/hub/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sparta.hub.infrastructure.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.hub.SecurityContextFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableMethodSecurity
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final ObjectMapper objectMapper;
+  @Bean
+  public SecurityContextFilter securityContextFilter() {
+    return new SecurityContextFilter(objectMapper);
+  }
+  @Bean
+  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            (sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(securityContextFilter(), UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
+
+}
