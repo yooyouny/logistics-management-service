13a4952f1ca266e58ba54533fa45fe99fde8c147
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-13 16:40:15 +0900

    add : Enum 클래스 Validation 구현

---
+++ service/user/user_server/src/main/java/com/sparta/user/presentation/controller/aop/ValidEnum.java
@@ -0,0 +1,18 @@
+package com.sparta.user.presentation.controller.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+  String message() default "Invalid value. This is not permitted.";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+  Class<? extends java.lang.Enum<?>> enumClass();
+}
