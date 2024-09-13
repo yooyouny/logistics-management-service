a5d792074c140e0df2fa60a6cf3dc736682db4ff
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-13 15:03:19 +0900

    fix: 중복되는 state 정리

--- service/delivery/delivery_server/src/main/java/com/sparta/delivery/domain/model/State.java
+++ service/delivery/delivery_server/src/main/java/com/sparta/delivery/domain/model/State.java
@@ -4,13 +4,10 @@ public class State {
 
   public enum DeliveryState {
     PENDING("배송대기"),
-    REQUESTED("배송요청"),
-    IN_TRANSIT_TO_HUB("허브이동중"),
-    AT_ARRIVAL_HUB("목적지허브도착"),
-    OUT_FOR_DELIVERY("출고완료"),
-    IN_TRANSIT_TO_COMPANY("업체배송중"),
-    DELIVERED("업체배송완료"),
-    CONFIRMED("업체배송확정");
+    REQUESTED("배송요청"),// 외부 API 전파없음
+    IN_TRANSIT("배송중"),
+    DELIVERED("배송완료"),
+    CONFIRMED("배송확정");
 
     final String description;
 
@@ -23,6 +20,7 @@ public class State {
     PENDING("허브이동대기중"),
     IN_TRANSIT_TO_HUB("허브이동중"),
     AT_ARRIVAL_HUB("목적지허브도착"),
+    OUT_FOR_DELIVERY("출고완료"),
     IN_TRANSIT_TO_COMPANY("업체배송중"),
     DELIVERED("배송완료");
     final String description;
