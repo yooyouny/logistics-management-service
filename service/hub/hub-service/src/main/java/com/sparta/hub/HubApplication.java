cc2ad4791473f25a35969b73bde361bb13236235
jdm2131 <jdm2131@naver.com>
2024-09-14 01:21:55 +0900

    fix: 설정 수정 #34

--- service/hub/hub-service/src/main/java/com/sparta/hub/HubApplication.java
+++ service/hub/hub-service/src/main/java/com/sparta/hub/HubApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableFeignClients
 public class HubApplication {
 
