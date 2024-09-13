c7f84e9c9230c10120483639161d41ab6b5d2516
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2024-09-13 16:40:39 +0900

    add : validation 적용

--- service/user/user_server/src/main/java/com/sparta/user/presentation/request/CreateShippingManagerRequest.java
+++ service/user/user_server/src/main/java/com/sparta/user/presentation/request/CreateShippingManagerRequest.java
@@ -1,12 +1,15 @@
 package com.sparta.user.presentation.request;
 
 import com.sparta.user.domain.model.vo.ShippingManagerType;
+import com.sparta.user.presentation.controller.aop.ValidEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record CreateShippingManagerRequest(
-    String slackId,
-    ShippingManagerType type,
-    UUID hubId
+    @NotBlank String slackId,
+    @ValidEnum(enumClass = ShippingManagerType.class) ShippingManagerType type,
+    @NotNull UUID hubId
 ) {
 
 }
--- service/user/user_server/src/main/java/com/sparta/user/presentation/request/UpdateAuthorityRequest.java
+++ service/user/user_server/src/main/java/com/sparta/user/presentation/request/UpdateAuthorityRequest.java
@@ -1,9 +1,10 @@
 package com.sparta.user.presentation.request;
 
 import com.sparta.user.domain.model.vo.UserRole;
+import com.sparta.user.presentation.controller.aop.ValidEnum;
 
 public record UpdateAuthorityRequest(
-    UserRole role
+    @ValidEnum(enumClass = UserRole.class) UserRole role
 ) {
 
 }
