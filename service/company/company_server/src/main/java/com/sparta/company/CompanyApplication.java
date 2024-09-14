8d562b7fb8f4382bcbdeae08c31ed074ded7c7f9
Gido <120196095+gidopa@users.noreply.github.com>
2024-09-14 21:54:07 +0900

    fix: 수정 #34

--- service/company/company_server/src/main/java/com/sparta/company/CompanyApplication.java
+++ service/company/company_server/src/main/java/com/sparta/company/CompanyApplication.java
@@ -3,8 +3,6 @@ package com.sparta.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
