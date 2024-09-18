a3f901d06732d33410c97639e9223234f90ac971
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-18 19:03:18 +0900

    add: 주문, 배송에 MethodArgumentTypeMismatchException 예외 핸들러 추가

--- service/delivery/delivery_server/src/main/java/com/sparta/delivery/presentation/exception/DeliveryControllerAdvice.java
+++ service/delivery/delivery_server/src/main/java/com/sparta/delivery/presentation/exception/DeliveryControllerAdvice.java
@@ -3,9 +3,12 @@ package com.sparta.delivery.presentation.exception;
 import com.sparta.commons.domain.exception.BusinessException;
 import com.sparta.commons.domain.response.FailedResponseBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j(topic = "DeliveryControllerAdvice")
@@ -13,9 +16,29 @@ public class DeliveryControllerAdvice {
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<?> businessExceptionHandler(BusinessException e){
     DeliveryErrorCode errorCode = (DeliveryErrorCode) e.getErrorCode();
-    log.error("Error occurs in DeliveryServer : {}", e.getErrorCode());
+    log.error("Error occurs in DeliveryServer : {}", e.toString());
     return ResponseEntity.status(errorCode.getHttpStatus())
         .body(new FailedResponseBody(errorCode.getCode(), errorCode.getMessage()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> validationExceptionHandler(final MethodArgumentNotValidException e) {
+    log.error("Validation error occurs in {}", e.toString());
+    String[] errorMessages =
+        e.getBindingResult().getFieldErrors().stream()
+            .map(error -> String.format("[%s]: %s", error.getField(), error.getDefaultMessage()))
+            .toArray(String[]::new);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new FailedResponseBody("common_error_002", errorMessages.toString()));
+  }
+
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<?> typeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
+    log.error("Type mismatch error occurs in {}", e.toString());
+    String errorMessage = String.format("Invalid value for %s: '%s'", e.getName(), e.getValue());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new FailedResponseBody("common_error_002",errorMessage));
   }
 
   @ExceptionHandler(RuntimeException.class)
--- service/order/order_server/src/main/java/com/sparta/order/presentation/exception/OrderControllerAdvice.java
+++ service/order/order_server/src/main/java/com/sparta/order/presentation/exception/OrderControllerAdvice.java
@@ -3,9 +3,11 @@ package com.sparta.order.presentation.exception;
 import com.sparta.commons.domain.exception.BusinessException;
 import com.sparta.commons.domain.response.FailedResponseBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j(topic = "OrderControllerAdvice")
@@ -16,6 +18,14 @@ public class OrderControllerAdvice {
     log.error("Error occurs in OrderServer : {}", e.getErrorCode());
     return ResponseEntity.status(errorCode.getHttpStatus())
         .body(new FailedResponseBody(errorCode.getCode(), errorCode.getMessage()));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<?> typeMismatchExceptionHandler(MethodArgumentTypeMismatchException e) {
+    log.error("Type mismatch error occurs in {}", e.toString());
+    String errorMessage = String.format("Invalid value for %s: '%s'", e.getName(), e.getValue());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new FailedResponseBody("common_error_002", errorMessage));
   }
 
   @ExceptionHandler(RuntimeException.class)
