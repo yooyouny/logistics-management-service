d8577ddb57a297c68b1b876f94471f6470414290
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-18 22:00:39 +0900

    fix: 리베이스 오류로 delivery 단일조회 컨트롤러 추가

--- service/delivery/delivery_server/src/main/java/com/sparta/delivery/presentation/DeliveryController.java
+++ service/delivery/delivery_server/src/main/java/com/sparta/delivery/presentation/DeliveryController.java
@@ -1,11 +1,16 @@
 package com.sparta.delivery.presentation;
 
-import com.sparta.commons.domain.response.ResponseBody;
 import com.sparta.commons.domain.response.SuccessResponseBody;
 import com.sparta.delivery.application.DeliveryFacadeService;
+import com.sparta.delivery.application.DeliveryService;
 import com.sparta.delivery.presentation.dto.DeliveryResponse;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/deliveries")
+@Validated
 public class DeliveryController {
   private final DeliveryFacadeService deliveryFacadeService;
+  private final DeliveryService deliveryService;
 
   @PostMapping("/{deliveryId}/requested")
-  public ResponseBody<DeliveryResponse> requestDeliveryAndCreateRoute(
+  public ResponseEntity<SuccessResponseBody<DeliveryResponse>> requestDeliveryAndCreateRoute(
       @PathVariable("deliveryId") UUID deliveryId) {
-    return new SuccessResponseBody<>(
-        deliveryFacadeService.requestDeliveryAndCreateRoute(deliveryId));
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(
+            new SuccessResponseBody<>(
+                deliveryFacadeService.requestDeliveryAndCreateRoute(deliveryId)));
+  }
+
+  @GetMapping("/{deliveryId}")
+  public SuccessResponseBody<DeliveryResponse> get(
+      @NotNull @PathVariable("deliveryId") UUID deliveryId) {
+    return new SuccessResponseBody<>(deliveryService.get(deliveryId));
   }
 }
