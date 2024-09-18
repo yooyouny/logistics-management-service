9cec3209eda4bda29b5d9964a74c3fcf0dada722
harper <35358294+yooyouny@users.noreply.github.com>
2024-09-18 21:59:07 +0900

    fix: 리베이스 오류로 내부API 조회 시 사용하는 쿼리 추가

--- service/order/order_server/src/main/java/com/sparta/order/infrastructure/repository/OrderRepository.java
+++ service/order/order_server/src/main/java/com/sparta/order/infrastructure/repository/OrderRepository.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
   Optional<Order> findByOrderId(UUID orderID);
+
+  Page<Order> findAllByManagementHubId(UUID managementHubId, Pageable pageable);
 
   @Query(
       "SELECT o FROM Order o WHERE o.receiverCompanyId = :companyId AND o.orderDate BETWEEN :startDate AND :endDate ORDER BY o.orderDate DESC")
